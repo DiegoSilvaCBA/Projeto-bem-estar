de4c96f5994807a64eee8e6b2fae43316c9d4e9d
Nirwine <fernandesssouza566@gmail.com>
2024-04-10 21:51:34 -0300

    Add post's /treinos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Cadastro = require('./cadastro.js');
+const CadastroTreino = require('./cadastroTreino.js');
 
 const app = express();
 const port = 3000;
@@ -16,8 +17,29 @@ app.post('/cadastro', (req, res) => {
       res.status(400).send('E-mail inválido. Por favor, insira um e-mail válido.');
     }
   });
-  
 
+}
+  
+app.post('/treinos', (req, res) => {
+  const { nome, data, hora } = req.body;
+
+  const novoTreino = cadastroTreino.adicionarTreino(nome, data, hora);
+
+  if (novoTreino) {
+      res.status(201).json(novoTreino);
+  } else {
+      res.status(400).send('Erro ao cadastrar o treino.');
+  }
+});
+
+app.get('/treinos', (req, res) => {
+  const treinos = cadastroTreino.listarTreinos();
+  res.json(treinos);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
