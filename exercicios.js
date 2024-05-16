3f55fc2b965d7026a1fd7493392cbe3e19b43446
João Vítor Fernandes <fernandesssouza566@gmail.com>
2024-05-15 23:11:16 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -3,6 +3,18 @@ class Exercicio {
         this.nome = nome;
         this.descricao = descricao;
     }
+
+    getNome() {
+        return this.nome;
+    }
+
+    getDescricao() {
+        return this.descricao;
+    }
+
+    setDescricao(novaDescricao) {
+        this.descricao = novaDescricao;
+    }
 }
 
 module.exports = Exercicio;
