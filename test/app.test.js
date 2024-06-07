7b9aeef0066481ccb85aa58234140f1a872c79a8
Tiago Antunes <ddhd746@gmail.com>
2024-06-07 12:31:44 -0300

    Implementando testes do cadastro com Jest e Super test

---
+++ test/app.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require('./app');
+//Teste do Cadastro//
+describe('POST /cadastro', () => {
+  it('Deve retornar 200 para um e-mail válido', async () => {
+    const res = await request(app)
+      .post('/cadastro')
+      .send({ nome: 'Nome', email: 'email@example.com', senha: 'senha' });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('valido', true);
+  });
+
+  it('Deve retornar 400 para um e-mail inválido', async () => {
+    const res = await request(app)
+      .post('cadastro')
+      .send({ nome: 'Nome', email: 'email@invalid', senha: 'senha' });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty('valido', false);
+  });
+});
